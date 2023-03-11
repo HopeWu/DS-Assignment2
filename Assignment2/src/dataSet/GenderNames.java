8d6857961e1b12f1a953fba25a73cb2b867392e8
HopeWu <30311646+HopeWu@users.noreply.github.com>
2023-03-11 08:51:41 +0000

    fix a gender bug

--- Assignment2/src/dataSet/GenderNames.java
+++ Assignment2/src/dataSet/GenderNames.java
@@ -7,28 +7,29 @@ import java.io.IOException;
 import java.util.HashMap;
 import java.util.Random;
 
-
 /**
  * To provide real name and gender services.
+ * 
  * @author haopengwu
  *
  */
 public class GenderNames {
-	
-	GenderNames(){
+
+	GenderNames() {
 		loadFile();
 	}
-		
+
 	public int size() {
 		return genderNames.size();
 	}
-	
+
 	// <name, gender>
 	private static HashMap<String, String> genderNames;
 	private static Random random = new Random();
 
 	private static void loadFile() {
-		if (genderNames != null) return;
+		if (genderNames != null)
+			return;
 		genderNames = new HashMap<String, String>();
 		BufferedReader in;
 		try {
@@ -37,8 +38,9 @@ public class GenderNames {
 			String[] row;
 			while ((line = in.readLine()) != null) {
 				row = line.split(",");
-				genderNames.put(row[0], row[1]);
+				genderNames.put(row[0].trim(), row[1].trim());
 			}
+			in.close();
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		} catch (IOException e) {
@@ -52,12 +54,12 @@ public class GenderNames {
 	 */
 	public Name getAMaleName() {
 		int index = random.nextInt(size());
-		while(genderNames.get(genderNames.keySet().toArray()[index]) == "f") {
+		while (!genderNames.get(genderNames.keySet().toArray()[index]).equals("m")) {
 			index = random.nextInt(size());
 		}
-		String name = (String)genderNames.keySet().toArray()[index];
+		String name = (String) genderNames.keySet().toArray()[index];
 		String gender = genderNames.get(name);
-		
+
 		return new Name(name, gender);
 	}
 
@@ -67,25 +69,26 @@ public class GenderNames {
 	 */
 	public Name getAFemaleName() {
 		int index = random.nextInt(size());
-		while(genderNames.get(genderNames.keySet().toArray()[index]) == "m") {
+		while (!genderNames.get(genderNames.keySet().toArray()[index]).equals("m")) {
 			index = random.nextInt(size());
 		}
-		String name = (String)genderNames.keySet().toArray()[index];
+		String name = (String) genderNames.keySet().toArray()[index];
 		String gender = genderNames.get(name);
-		
+
 		return new Name(name, gender);
 	}
 
 	/**
 	 * randomly get a name no matter if it's of male or female
+	 * 
 	 * @return
 	 */
 	public Name getAName() {
 		int index = random.nextInt(size());
 
-		String name = (String)genderNames.keySet().toArray()[index];
+		String name = (String) genderNames.keySet().toArray()[index];
 		String gender = genderNames.get(name);
-		
+
 		return new Name(name, gender);
 	}
 }
