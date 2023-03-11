0894b9cc51eb7569982e7b13eced011e6bc4cf05
HopeWu <30311646+HopeWu@users.noreply.github.com>
2023-03-11 01:14:47 +0000

    Name class dev

---
+++ Assignment2/src/dataSet/Name.java
@@ -0,0 +1,18 @@
+package dataSet;
+
+public class Name {
+	private String name;
+	private String gender;
+	
+	public Name(String name, String gender) {
+		this.name = name;
+		this.gender = gender;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	public String getGender() {
+		return gender;
+	}
+}
