69aab66e63d0141524a2739fea4d4ca30d8f5dbc
HopeWu <30311646+HopeWu@users.noreply.github.com>
2023-03-11 01:14:23 +0000

    DNA class dev

---
+++ Assignment2/src/dataSet/DNA.java
@@ -0,0 +1,27 @@
+package dataSet;
+
+import java.util.Random;
+
+public class DNA {
+	private static final Character[] ACGT = {'A','C','G','T'};
+	private static final int length = 22;
+
+	/**
+	 * Do not allow creating instances
+	 */
+	private DNA() {
+	}
+
+	/**
+	 * 
+	 * @return a random dna
+	 */
+	static public String get() {
+		String dna = "";
+		Random random = new Random();
+		for(int i = 0; i < length; ++i) {
+			dna += ACGT[random.nextInt(ACGT.length)];
+		}
+		return dna;
+	}
+}
