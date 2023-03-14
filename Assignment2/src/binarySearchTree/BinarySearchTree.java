7c00ea61dd66292bb8cb53f1389a7dae5065d9c7
HopeWu <30311646+HopeWu@users.noreply.github.com>
2023-03-14 13:27:57 +0000

    complete lookup function

--- Assignment2/src/binarySearchTree/BinarySearchTree.java
+++ Assignment2/src/binarySearchTree/BinarySearchTree.java
@@ -7,17 +7,17 @@ import personPackage.DNA;
 import tree.Tree;
 
 public class BinarySearchTree extends Tree {
-	
+
 	public static void main(String[] args) {
 		/**
 		 * get testing dataset
 		 */
 		Dataset dataset = new Dataset(100);
 		Person[] people = dataset.getData();
-		
+
 		BinarySearchTree bst = new BinarySearchTree();
 		bst.buildTree(people);
-		
+
 		/**
 		 * try some lookups to verify
 		 */
@@ -26,17 +26,11 @@ public class BinarySearchTree extends Tree {
 		System.out.println(people[2]);
 		System.out.println(person);
 	}
-	
-	public Person lookup(DNA dna) {
-		// TODO Auto-generated method stub
-		return null;
-	}
 
 	private BSTNode root = null;
-	
+
 	/**
-	 * Build a tree from a family tree
-	 * @personData, the root of the family tree
+	 * Build a tree from a family tree @personData, the root of the family tree
 	 */
 	public Node buildTree(Person[] people) {
 		for (int i = 0; i < people.length; ++i) {
@@ -52,18 +46,18 @@ public class BinarySearchTree extends Tree {
 	public void insertPerson(Person person) {
 		this._insertPerson(root, person);
 	}
-	
+
 	public void _insertPerson(BSTNode root, Person person) {
 		if (this.root == null) {
 			// if current node is null, insert into it
 			root = new BSTNode(person);
 			return;
 		}
-		
+
 		if (person.dna.isGreaterThan(root.data.dna)) {
 			// search right child
 			this._insertPerson(root.right, person);
-		}else {
+		} else {
 			// search left child
 			this._insertPerson(root.left, person);
 		}
@@ -71,18 +65,38 @@ public class BinarySearchTree extends Tree {
 
 	public void insert(Person[] persons) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	public void insertFamily(Person person) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
+	public Person lookup(DNA dna) {
+
+		return _lookup(root, dna);
+	}
+
+	public Person _lookup(BSTNode root,DNA dna) {
+		if (root == null)
+			// didn't find it
+			return null;
+		if (root.data.dna.isEqualTo(dna)) {
+			// found it!
+			return root.data;
+		}
+		if (root.data.dna.isGreaterThan(dna)) {
+			// search its right child
+			return this._lookup(root.right, dna);
+		}else {
+			// search its left child
+			return this._lookup(root.left, dna);
+		}
+	}
 
 	public int getHeight() {
 		// TODO Auto-generated method stub
 		return 0;
 	}
 }
-
