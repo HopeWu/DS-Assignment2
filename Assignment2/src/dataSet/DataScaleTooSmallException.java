c5750a71d2d72f2195050aa11901c789caa4e6d8
HopeWu <30311646+HopeWu@users.noreply.github.com>
2023-03-18 21:21:17 +0000

    add more comments

--- Assignment2/src/dataSet/DataScaleTooSmallException.java
+++ Assignment2/src/dataSet/DataScaleTooSmallException.java
@@ -1,5 +1,11 @@
 package dataSet;
 
+
+/**
+ * We don't allow the datasize be too small. Specifically, smaller than 6.
+ * @author haopengwu
+ *
+ */
 public class DataScaleTooSmallException extends Exception {
 	DataScaleTooSmallException(String errorMessage){
 		super(errorMessage);
